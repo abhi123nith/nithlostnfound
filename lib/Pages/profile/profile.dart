82d4ae969254099b6855e99c89b7c3f0656937ad
abhi123nith <22bcs007@nith.ac.in>
2024-09-13 23:13:30 +0530

    commit

--- lib/Pages/profile/profile.dart
+++ lib/Pages/profile/profile.dart
@@ -4,7 +4,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // For date formatting
+import 'package:intl/intl.dart'; 
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
